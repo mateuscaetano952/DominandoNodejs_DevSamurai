01e78ff941230702e029a5dbd82295761cea4741
Mateus Caetano <mateuscaetano952@gmail.com>
2025-03-02 08:54:14 -0300

    sequelize configurado

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,14 @@
+const { underscoredIf } = require("sequelize/lib/utils");
+
 module.exports = {
-    dialect: 'postgres',
+    dialect: "postgres",
     host: "localhost",
     username: "postgres",
     password: "secret",
-    database: ""
+    database: "teste-dominando-nodejs",
+    define:{
+        timestamp: true, // Cria duas coluna update_at e created_at
+        underscored: true, //troca a nomeclatura =>  nomeTeste1 => nome_teste_1 =
+        underscoredAll: true
+    }
 }
